141f588ee585fd593b129cc91c0702acdf14b5d1
muhammadsofiullah <muhammad.sofiullah@maleo.social>
2021-04-01 03:12:40 +0700

    fix navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,7 +41,9 @@
                 </div>
             </nav>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <h1>MAIN</h1>
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1>MAIN</h1>
+                </div>
             </main>
         </div>
     </div>
