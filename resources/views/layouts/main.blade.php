aa9f2a069187c9502b1c837c0f8337fee3ca3b02
muhammadsofiullah <muhammad.sofiullah@maleo.social>
2021-04-01 03:15:27 +0700

    fix navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,12 +19,7 @@
         <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 text-center" href="#">{{config('app.name', 'Simple Task Report')}}</a>
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
+        </button>
     </header>
     <div class="container-fluid">
         <div class="row">
