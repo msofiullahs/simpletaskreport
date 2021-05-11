bf1c6df561fe7ec958fe457c21c2c07b8c8edf88
muhammadsofiullah <muhammad.sofiullah@maleo.social>
2021-05-12 01:13:53 +0700

    update print

--- resources/views/user/print.blade.php
+++ resources/views/user/print.blade.php
@@ -9,7 +9,10 @@
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
         <style>
-            body {margin: 20px}
+            body {
+                -webkit-print-color-adjust:exact;
+                margin: 20px
+            }
             tr.weekend {
                 background: #00695c !important;
                 color: #fff !important;
