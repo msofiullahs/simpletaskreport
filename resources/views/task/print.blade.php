a7a16b7b11a6c673fe450a17e004f3864a3cd418
muhammadsofiullah <muhammad.sofiullah@maleo.social>
2021-05-19 01:20:48 +0700

    test print preview

--- resources/views/task/print.blade.php
+++ resources/views/task/print.blade.php
@@ -82,7 +82,10 @@
         </style>
     </head>
     <body>
-        <h2 class="text-center">{{\Carbon\Carbon::parse(request()->columns[0]['search']['value'])->format('F Y')}}</h2>
+        @php
+            dd(\Carbon\Carbon::parse(request()->columns[0]['search']['value'])->format('F Y'));
+        @endphp
+        {{-- <h2 class="text-center">{{Carbon\Carbon::parse(request()->columns[0]['search']['value'])->format('F Y')}}</h2> --}}
         <table class="table table-bordered table-condensed table-striped">
             @foreach($data as $row)
                 @if ($loop->first)
